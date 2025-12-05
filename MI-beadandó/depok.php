5e0bbc775f20a183aee62b745e79fc1cd64e4a09
Orosz Kristóf <kristof.orosz.11@gmail.com>
2025-12-05 11:16:11 +0100

    Add files via upload

--- MI-beadandó/depok.php
+++ MI-beadandó/depok.php
@@ -1,25 +1,25 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-// Betöltöm az adatbázis-kapcsolatot a kapcsolat.php fájlból.
+
 require_once 'kapcsolat.php'; 
 
-// Lekérdezem az összes depót az adatbázisból a szükséges mezőkkel.
+
 $sql = "SELECT id, nev, x, y, aktualis, cel FROM depok";
 $result = $conn->query($sql);
 
-// Egy üres tömböt hozok létre, amibe a lekérdezett adatokat fogom eltárolni.
+
 $depok = [];
 
-// Ha a lekérdezés sikeres és van eredmény, bejárom a sorokat és beleteszem a tömbbe.
+
 if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $depok[] = $row;
     }
-    // A PHP tömböt JSON formátumba alakítom és visszaküldöm a kliensnek.
+   
     echo json_encode($depok, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 } else {
-    // Ha nincs adat, hibaüzenetet küldök JSON formátumban.
+    
     echo json_encode(["error" => "Nem található adat a depok táblában."]);
 }
 
--- MI-beadandó/kapcsolat.php
+++ MI-beadandó/kapcsolat.php
@@ -1,17 +1,17 @@
 <?php
-// Megadom az adatbázis-kapcsolat beállításához szükséges adatok megadása.
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "MI_Beadando";
 
-// Létrehozom a kapcsolatot a MySQL adatbázissal a mysqli osztály segítségével.
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 
 $conn->set_charset("utf8mb4");
 
-// Ellenőrzöm, hogy sikerült-e a kapcsolat.
+
 if ($conn->connect_error) {
     die("Adatbázis kapcsolat sikertelen: " . $conn->connect_error);
 }
